c37f6a221144489bcd6da2f6e628c9bc192d5c58
biazidioti <bzferreira.bio@gmail.com>
2021-11-30 09:01:55 -0300

    Register validation

--- front-end/src/components/Cadastro.jsx
+++ front-end/src/components/Cadastro.jsx
@@ -1,12 +1,47 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import genHashMd5 from 'md5';
+import { Link, useNavigate } from 'react-router-dom';
+import registerValidation from '../validations/registerValidation';
+import api from '../services/api';
+import ErrorBackend from './ErrorBackend/index';
 
 export default function Cadastro() {
   const [nameInput, setNameInput] = useState('');
   const [emailInput, setEmailInput] = useState('');
   const [passwordInput, setPasswordInput] = useState('');
-  // const [disabledBtn, setDisabledBtn] = useState(true);
-  // const [errorRegsiter, setErrorRegister] = useState(false);
+  const [disabledBtn, setDisabledBtn] = useState(true);
+  const [errorRegsiter, setErrorRegister] = useState(false);
+
+  const errorLoginHTTP = 400;
+  const navegate = useNavigate();
+
+  const fetchPostData = async (userData) => {
+    try {
+      const data = await api.post('/register', userData);
+      if (data.status > errorLoginHTTP) {
+        setErrorLogin(true);
+        return;
+      }
+      localStorage.setItem('token', data.data.token);
+      localStorage.setItem('email', data.data.email);
+      localStorage.setItem('role', data.data.role);
+      localStorage.setItem('name', data.data.name);
+      const redirectRoutes = {
+        customer: '/customer/products',
+        seller: '/seller/orders',
+        administrator: '/admin/manage',
+      };
+      navegate(redirectRoutes[data.data.role]);
+    } catch (error) {
+      setErrorRegister(true);
+    }
+  };
+
+  const handleButtonClick = async () => {
+    const passwordHash = genHashMd5(passwordInput);
+    await fetchPostData({ name: nameInput, email: emailInput, password: passwordHash });
+    console.log('handleButtonClick');
+  };
 
   return (
     <div>
@@ -19,6 +54,7 @@ export default function Cadastro() {
             placeholder="Seu nome"
             onChange={ (e) => {
               setNameInput(e.target.value);
+              setDisabledBtn(!registerValidation(nameInput, emailInput, passwordInput));
             } }
           />
         </label>
@@ -30,6 +66,7 @@ export default function Cadastro() {
             placeholder="seu email@site.com.br"
             onChange={ (e) => {
               setEmailInput(e.target.value);
+              setDisabledBtn(!registerValidation(nameInput, emailInput, passwordInput));
             } }
           />
         </label>
@@ -41,19 +78,28 @@ export default function Cadastro() {
             placeholder="*******"
             onChange={ (e) => {
               setPasswordInput(e.target.value);
+              setDisabledBtn(!registerValidation(nameInput, emailInput, passwordInput));
             } }
           />
         </label>
         <h2>{passwordInput}</h2>
-        <Link to="/user">
+        <Link to="/customer/products">
           <button
             type="button"
-            common_register__button-register
+            data-testid="common_register__button-register"
+            onClick={ handleButtonClick }
+            disabled={ disabledBtn }
           >
             Cadastrar
           </button>
         </Link>
       </form>
+      {
+        errorRegsiter ? <ErrorBackend
+          datatestid="common_register__element-invalid_register"
+          messageError="Pane no sistema"
+        /> : null
+      }
     </div>
   );
 }
