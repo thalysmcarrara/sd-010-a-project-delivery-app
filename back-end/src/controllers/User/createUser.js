6b70145d894b10b842469f123296be4424bc5ca2
Anderson Nascimento <anderson.nascimentoafsn@gmail.com>
2021-11-30 17:25:57 -0300

    fix: Mudança do md5 do password do front para o back-end

--- back-end/src/controllers/User/createUser.js
+++ back-end/src/controllers/User/createUser.js
@@ -1,17 +1,20 @@
+const genHashMd5 = require('md5');
+
 const insert = require('../../services/insert');
 const find = require('../../services/find');
 const { isUserExists } = require('../../schemas');
 
 module.exports = async (req, res) => {
   const { name, email, password, role = 'customer' } = req.body;
-  
+  const passwordHash = genHashMd5(password);
+
   const isExists = await isUserExists({ name, email }, find);
   
   if (isExists.message) return res.status(isExists.status).json(isExists.message);
 
   const createdUser = await insert(
     'users',
-    { name, email, password, role },
+    { name, email, password: passwordHash, role },
   );
 
   res.status(201).json(createdUser);
