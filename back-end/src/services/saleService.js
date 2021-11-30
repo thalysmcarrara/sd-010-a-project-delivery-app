3818f44d6ebc9018779fd7bdee112ef0454cc975
rafael <rafittu@outlook.com>
2021-11-30 13:11:03 -0300

    feat req 21

--- back-end/src/services/saleService.js
+++ back-end/src/services/saleService.js
@@ -1,21 +1,25 @@
 const moment = require('moment');
 const { Sale, SaleProduct } = require('../database/models');
 
+
 const create = async (order, id) => {
   const date = moment().format('MM/DD/YYYY HH:mm:ss');
-
   const sale = await Sale.create({ ...order, sale_date: date, user_id: id, status: 'Pendente' });
   if (!sale) return { status: 500, message: 'Internal Server Error' };
 
-  const sale_id = sale.dataValues.id;
-  const quantity = order.products[0].quantity;
-  const product_id = order.products[0].productId;
+  console.log(sale);
+  
+  const { products } = order;
+  const saleId = sale.dataValues.id;
 
-  const createSaleProduct = await SaleProduct.create({ sale_id, quantity, product_id });
-  if (!createSaleProduct) return { status: 500, message: 'Sale not created' };
-
-  console.log(createSaleProduct);
-
+  products.forEach(async (product) => {
+    const createSaleProduct = await SaleProduct.create({
+      sale_id: saleId,
+      product_id: product.productId,
+      quantity: product.quantity,
+    });
+    if (!createSaleProduct) return { status: 500, message: 'Sale not created' };
+  });
   return { saleId: sale.dataValues.id, status: 201, sale };
 };
 
