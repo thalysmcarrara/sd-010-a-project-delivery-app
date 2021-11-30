6b118ec141030caaa25cd2b7fb564cf783d86d2c
Marcelofcdantas <65692996+Marcelofcd@users.noreply.github.com>
2021-11-30 07:51:22 -0300

    fix: requisito 31 aprovado

--- front-end/src/pages/SellerOrderDetails.js
+++ front-end/src/pages/SellerOrderDetails.js
@@ -2,12 +2,25 @@ import React, { useState, useEffect } from 'react';
 // import { useParams } from 'react-router';
 import { getSaleBySellerId } from '../utils/Data';
 import dateFormatation, { priceFormat } from '../utils/Format';
-// import Header from '../Components/Header';
+import Header from '../Components/Header';
 
 export default function SellerOrderDetails() {
   const [order, setOrders] = useState([]);
   const [inProgress] = useState(false);
   const [orderReady] = useState(false);
+
+  const LINKS = [
+    {
+      name: 'PRODUTOS',
+      url: '/customer/products',
+      testId: 'customer_products__element-navbar-link-products',
+    },
+    {
+      name: 'MEUS PEDIDOS',
+      url: '/customer/orders',
+      testId: 'customer_products__element-navbar-link-orders',
+    },
+  ];
 
   const dataTestIds = {
     labelOrderId: 'seller_order_details__element-order-details-label-order-id',
@@ -24,8 +37,13 @@ export default function SellerOrderDetails() {
     totalPrice: 'seller_order_details__element-order-total-price',
   };
 
-  const handleClick = () => {
-    console.log(order[0].id);
+  // const handleClick = () => {
+  //   console.log(order[0].id);
+  // };
+
+  const changeStatus = async (saleId) => {
+    await statusChange(saleId);
+    return update === false ? setUpdate(true) : setUpdate(false);
   };
 
   useEffect(() => {
@@ -40,38 +58,40 @@ export default function SellerOrderDetails() {
   // const { sale } = order;
   return (
     <div>
+      <Header links={ LINKS } />
       <h1>Detalhes do pedido - vendedor</h1>
       {!order
         ? 'Loading'
         : order.map((sale) => (
-          <div className="card-top-bar" key={ sale.id }>
-            <div data-testid={ dataTestIds.labelOrderId }>{sale.id}</div>
-            <div
-              data-testid={ dataTestIds
-                .orderDate }
-            >
-              {dateFormatation(sale.saleDate)}
-
-            </div>
-            <div data-testid={ dataTestIds.deliveryStatus }>{sale.status}</div>
-            <button
-              data-testid={ dataTestIds.buttonPreparingCheck }
-              name="prepare-order"
-              disabled={ inProgress }
-              onClick={ handleClick }
+          <>
+            <div className="card-top-bar" key={sale.id}>
+              <div data-testid={dataTestIds.labelOrderId}>{sale.id}</div>
+              <div
+                data-testid={dataTestIds
+                  .orderDate}
+              >
+                {dateFormatation(sale.saleDate)}
+              </div>
+              <div data-testid={dataTestIds.deliveryStatus}>{sale.status}</div>
+              <button
+                data-testid={dataTestIds.buttonPreparingCheck}
+                name="prepare-order"
+                disabled={inProgress}
+                onClick={ ()=> changeStatus(item.id) }
               type="button"
-            >
+              >
               PREPARAR PEDIDO
-            </button>
-            <button
-              data-testid={ dataTestIds.buttonDispatchCheck }
-              name="deliver-order"
-              disabled={ !orderReady }
-              onClick={ handleClick }
-              type="button"
-            >
-              SAIU PARA ENTREGA
-            </button>
+              </button>
+              <button
+                data-testid={dataTestIds.buttonDispatchCheck}
+                name="deliver-order"
+                disabled={!orderReady}
+                onClick={ ()=> changeStatus(item.id) }
+                type="button"
+              >
+                SAIU PARA ENTREGA
+              </button>
+            </div>
             <div className="card-list">
               <ul>
                 {sale.products.map((product, index) => (
@@ -105,10 +125,10 @@ export default function SellerOrderDetails() {
                 ))}
               </ul>
               <div data-testid={ priceFormat(dataTestIds.totalPrice) }>
-                {sale.totalPrice}
+                {priceFormat(sale.totalPrice)}
               </div>
             </div>
-          </div>
+          </>
         ))}
     </div>
   );
