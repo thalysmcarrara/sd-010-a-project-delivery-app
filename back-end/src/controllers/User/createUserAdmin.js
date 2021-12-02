23fe6133c612bf3637a113c804280510a8ad47cc
Anderson Nascimento <anderson.nascimentoafsn@gmail.com>
2021-12-02 13:07:23 -0300

    fix: token invalid

--- back-end/src/controllers/User/createUserAdmin.js
+++ back-end/src/controllers/User/createUserAdmin.js
@@ -2,19 +2,28 @@ const genHashMd5 = require('md5');
 const insert = require('../../services/insert');
 const find = require('../../services/find');
 const { isUserExists } = require('../../schemas');
+const takeToken = require('../../services/takeToken');
+// const { passwordToken } = require('../../services');
 
 module.exports = async (req, res) => {
   const { name, email, password, role = 'customer' } = req.body;
+  const { authorization } = req.headers;
+
   const passwordHash = genHashMd5(password);
   
   const isExists = await isUserExists({ name, email }, find);
   
   if (isExists.message) return res.status(isExists.status).json(isExists.message);
 
-  const createdUser = await insert(
-    'users',
-    { name, email, password: passwordHash, role },
-  );
+  const payload = takeToken(authorization);
+  if (payload.role === 'administrator') {
+    const createdUser = await insert(
+      'users',
+      { name, email, password: passwordHash, role },
+    );
+    // const tokenGen = passwordToken({ id: createdUser.id, role: createdUser.role });
+    return res.status(201).json(createdUser);
+  }
 
-  res.status(201).json(createdUser);
+  res.status(400).json({ message: 'Invalid authorization' });
 };
