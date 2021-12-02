d290c1cfdbb2ef5d8a3776af154af10b173f5741
Anderson Nascimento <anderson.nascimentoafsn@gmail.com>
2021-12-02 13:08:38 -0300

    fix: token invalid

--- back-end/src/services/takeToken.js
+++ back-end/src/services/takeToken.js
@@ -1,10 +1,17 @@
-const jwt = require('jsonwebtoken');
 require('dotenv').config();
+const jwt = require('jsonwebtoken');
+const path = require('path');
 
-const SECRET = process.env.SECRET || 'segredo';
+// const SECRET_KEY = process.env.SECRET || 'segredo';
+
+const SECRET_KEY = require('fs')
+  .readFileSync(
+    path.join(__dirname, '..', '..', 'jwt.evaluation.key'),
+    { encoding: 'utf-8' },
+  ).trim();
 
 const takeToken = (token) => {
-    const { payload } = jwt.verify(token, SECRET);
+    const { payload } = jwt.verify(token, SECRET_KEY);
     return payload;
 };
 
