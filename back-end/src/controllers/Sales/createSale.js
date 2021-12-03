70f2593fac663fb39915d1e37478b637e1b3ed0f
Anderson Nascimento <anderson.nascimentoafsn@gmail.com>
2021-12-03 16:51:23 -0300

    atualiza createSale para preencher a tabela intermediária

--- back-end/src/controllers/Sales/createSale.js
+++ back-end/src/controllers/Sales/createSale.js
@@ -3,15 +3,22 @@ const { insert } = require('../../services');
 // const { insert, takeToken } = require('../../services');
 
 const createSale = async (req, res) => {
-  console.log('controller');
   const date = moment().locale('pt-br').format('DD/MM/YYYY');
 
-  // const { authentication } = req.headers;
   const dataSale = req.body;
-  // console.log(dataSale);
-  // const { id } = takeToken(authentication);
-  // if (!id) return res.status(400).json('Bad request');
+
   const sale = await insert('sales', { ...dataSale, status: 'Pendente', saleDate: date });
+  
+  const refined = sale.get({ plain: true });
+  
+  await Promise.all(dataSale.products.map(async (product) => {
+    const data = {
+      saleId: refined.id,
+      productId: product.id,
+      quantity: product.quantity,
+    };
+    return insert('salesProducts', data);
+  }));
   
   return res.status(201).json(sale);
 };
