fbed13ec0cb66f7964f584d03c94ce63cb1bbbb0
Micaela Alves <micaela.alves93@gmail.com>
2021-12-08 15:48:27 -0300

    fix lint

--- back-end/src/controllers/Sales/getAllSales.js
+++ back-end/src/controllers/Sales/getAllSales.js
@@ -1,62 +1,51 @@
-const { find, takeToken } = require('../../services');
+const { takeToken } = require('../../services');
 const { Product, User, SalesProduct, Sale } = require('../../database/models');
+
+const findProd = async (idProd, quantity) => {
+  const product = await Product.findOne({
+    where: { id: idProd },
+  });
+
+  const { name } = product;
+
+  return { name, quantity, price: product.price, total: quantity * product.price };
+};
+
+function formatData(data) {
+  return {
+    id: data.id,
+    sellerName: data.name,
+    saleDate: data.saleDate,
+    status: data.status,
+    totalPrice: data.totalPrice,
+    products: data.products,
+  };
+}
 
 const getAllSales = async (req, res) => {
   const { authorization } = req.headers;
   const { id } = req.params;
-  // console.log('id', id);
-  console.log('getsales');
+
   const payload = takeToken(authorization);
-  if(!payload) return res.status(400).json({ message: 'Token inválido'});
-  // console.log('payload:::', payload);
-  // const { id } = payload;
-  // const sales = await find('salesProducts', 
-  //     { saleId: id },
-  //     { attributes: { exclude: ['sellerId']}},
-  // );
+  if (!payload) return res.status(400).json({ message: 'Token inválido' });
+  
   const sales = await Sale.findOne({
    where: { id },
   });
-  // console.log('sales:::', sales);
-  // const salesFiltered = payload.role === 'customer'
-  //   ? sales.filter((sale) => sale.userId === payload.id)
-  //   : sales.filter((sale) => sale.sellerId === payload.id);
+  
   const { sellerId } = sales;
   const seller = await User.findOne({
     where: { id: sellerId },
    });
-  //  console.log('seller:::', seller.name);
+  const prods = await SalesProduct.findAll({ where: { saleId: id } });
 
-  const prods = await SalesProduct.findAll({
-    where: { saleId: id }
-  });
-  const findProd = async (idProd, quantity) => {
-    const product = await Product.findOne({
-      where: { id: idProd },
-    });
-    console.log('produuuuuuuuuu', product);
-    const name = product.name;
+  const products = await Promise.all(prods.map((pr) => findProd(pr.productId, pr.quantity)));
 
-     // console.log(ppp.name);
-    return { name, quantity, price: product.price, total: quantity * product.price }
-  };
+  const { saleDate, status, totalPrice } = sales;
 
-  const products = await Promise.all(prods.map((pr) => 
-    findProd(pr.productId, pr.quantity)
-  ));
+  const params = { id, name: seller.name, saleDate, status, totalPrice, products };
 
-  // console.log('Prod:::', products);
-
-  const retornoDetalhe = {
-    id,
-    'sellerName': seller.name,
-    'saleDate': sales.saleDate,
-    'status': sales.status,
-    'totalPrice': sales.totalPrice,
-    products,
-  };
-
-  return res.status(200).json(retornoDetalhe);
+  return res.status(200).json(formatData(params));
 };
 
 module.exports = getAllSales;
