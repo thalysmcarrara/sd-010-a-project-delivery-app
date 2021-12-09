dd2b53e02df65a86bf164737ec637ff9ffe3c23f
Gui-lira <gui.lira.martins@gmail.com>
2021-12-09 18:40:45 -0300

    test 08 09 e 10

--- front-end/src/components/CardOrdersSeller/index.jsx
+++ front-end/src/components/CardOrdersSeller/index.jsx
@@ -9,22 +9,21 @@ import formatPrice from '../../utils/formatPrice';
 export default function CardOrdersSeller() {
   const { id } = useParams();
   const socket = io('http://localhost:3001');
-  const [sales, setSale] = useState({});
+  const [sale, setSale] = useState({});
   useEffect(() => {
     socket.emit('getSale', id);
     socket.on('takeSale', (response) => {
       setSale(response);
     });
   }, []);
-  // const onClick = ({ target }) => {
-  //   const statusValue = { preparo: 'Preparando',
-  //     'saiu para entrega': 'Em Trânsito',
-  //     Entregue: 'Entregue' };
-  //   const status = statusValue[target.value];
-  //   console.log(status);
-  //   socket.emit('sendStatus', { id, status });
-  // };
-  if (!sales) {
+  const onClick = ({ target }) => {
+    // const statusValue = { preparo: 'Preparando',
+    //   'saiu para entrega': 'Em Trânsito',
+    //   Entregue: 'Entregue' };
+    // const status = statusValue[target.value];
+    socket.emit('sendStatus', { id, status: target.value });
+  };
+  if (!sale) {
     return <p>Loading ... </p>;
   }
   const delivery = 'seller_order_details__element-order-details-label-delivery-status';
@@ -36,42 +35,47 @@ export default function CardOrdersSeller() {
         <h3
           data-testid="seller_order_details__element-order-details-label-order-id"
         >
-          {`PEDIDO 0${sales.id}; `}
+          {`PEDIDO 0${sale.id}; `}
         </h3>
         <h3
           data-testid="seller_order_details__element-order-details-label-order-date"
         >
-          {moment(sales.saleDate).format('DD/MM/YYYY')}
+          {moment(sale.saleDate).format('DD/MM/YYYY')}
         </h3>
         <div>
           <h1
             data-testid={ delivery }
           >
-            {sales.status}
+            {sale.status}
           </h1>
         </div>
         <button
           type="button"
           data-testid="seller_order_details__button-preparing-check"
+          value="Preparando"
+          disabled={ sale.status !== 'Pendente' }
+          onClick={ onClick }
         >
           PREPARAR PEDIDO
         </button>
         <button
           type="button"
           data-testid="seller_order_details__button-dispatch-check"
-          disabled
+          value="Em Trânsito"
+          disabled={ sale.status !== 'Preparando' }
+          onClick={ onClick }
         >
           SAIU PARA ENTREGA
         </button>
       </div>
       <div>
-        { sales.products
-          ? <OrdersTable orderList={ sales.products } /> : null }
+        { sale.products
+          ? <OrdersTable orderList={ sale.products } /> : null }
         <h1>{'Total: R$ '}</h1>
         <h1
           data-testid="seller_order_details__element-order-total-price"
         >
-          { formatPrice(sales.totalPrice) }
+          { formatPrice(sale.totalPrice) }
         </h1>
       </div>
     </div>
