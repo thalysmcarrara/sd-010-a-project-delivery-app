0c0948e00b556bf61ba73bf18385b8f7d61849b2
Gui-lira <gui.lira.martins@gmail.com>
2021-12-09 15:34:13 -0300

    socket feito

--- back-end/src/services/getSaleById.js
+++ back-end/src/services/getSaleById.js
@@ -1,21 +1,43 @@
-const { Sale, SalesProduct, Product } = require('../database/models');
+const { Sale, SalesProduct, Product, User } = require('../database/models');
+
+const findProd = async (idProd, quantity) => {
+    const product = await Product.findOne({
+      where: { id: idProd },
+    });
+  
+    const { name } = product;
+  
+    return { name, quantity, price: product.price, total: quantity * product.price };
+  };
+  
+  function formatData(data) {
+    return {
+      id: data.id,
+      sellerName: data.name,
+      saleDate: data.saleDate,
+      status: data.status,
+      totalPrice: data.totalPrice,
+      products: data.products,
+    };
+  }
 
 const getSaleById = async (id) => {    
-    const rawResults = await Sale.findOne({
-        raw: true,
-        nest: true,
-         where: { id },         
-          include: [{ model: SalesProduct, include: [{ model: Product }] }] });   
-    console.log(rawResults);
-    const modeled = { ...rawResults,
-        products: rawResults.SalesProducts.map((product) => ({
-                name: product.Products.name,
-                price: product.Products.price,
-                quantity: product.quantity,
-            })),
-        };
-    delete modeled.SalesProducts;
-    return modeled;        
+    const sales = await Sale.findOne({
+        where: { id },
+       });       
+       const { sellerId } = sales;
+       const seller = await User.findOne({
+         where: { id: sellerId },
+        });
+       const prods = await SalesProduct.findAll({ where: { saleId: id } });
+     
+       const products = await Promise.all(prods.map((pr) => findProd(pr.productId, pr.quantity)));
+     
+       const { saleDate, status, totalPrice } = sales;
+     
+       const params = { id, name: seller.name, saleDate, status, totalPrice, products };
+       
+       return formatData(params);
 };
 
 module.exports = getSaleById;
