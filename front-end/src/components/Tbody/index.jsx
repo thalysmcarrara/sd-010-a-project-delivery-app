259c0f77b1a74b3da4260c53c2f1d88d4254ce62
Anderson Nascimento <anderson.nascimentoafsn@gmail.com>
2021-12-09 08:31:33 -0300

    feat: elementos movidos para a pasta componentes

---
+++ front-end/src/components/Tbody/index.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Td from './td';
+import ButtonTable from '../ButtonTable';
+
+const Tbody = ({ tbodyList }) => (
+  <tbody>
+    {
+      tbodyList.map(({ id, email, name, role }, index) => (
+        <tr key={ `${index}_${name}` }>
+          <Td
+            data={ id }
+            dataTestId={ `admin_manage__element-user-table-item-number-<${index}>}` }
+          />
+
+          <Td
+            data={ name }
+            dataTestId={ `admin_manage__element-user-table-name-<${index}>` }
+          />
+
+          <Td
+            data={ email }
+            dataTestId={ `admin_manage__element-user-table-email-<${index}>` }
+          />
+
+          <Td
+            data={ role }
+            dataTestId={ `admin_manage__element-user-table-role-<${index}>` }
+          />
+
+          <ButtonTable
+            dataTestId={ `admin_manage__element-user-table-remove-<${index}>` }
+            data="Excluir"
+          />
+        </tr>
+      ))
+    }
+  </tbody>
+);
+
+Tbody.propTypes = {
+  tbodyList: PropTypes.arrayOf(PropTypes.shape({
+    id: PropTypes.number,
+    name: PropTypes.string,
+    email: PropTypes.string,
+    password: PropTypes.string,
+  })).isRequired,
+};
+
+export default Tbody;
