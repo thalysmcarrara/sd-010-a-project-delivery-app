d189546c2dbdcb70c86036393d9bbbcca24e1fda
Anderson Nascimento <anderson.nascimentoafsn@gmail.com>
2021-12-09 08:29:27 -0300

    refactor: adiciona lista de usuários do estado global ao componente

--- front-end/src/pages/AdminManagerUsers/TableRegisteredUsers/index.jsx
+++ front-end/src/pages/AdminManagerUsers/TableRegisteredUsers/index.jsx
@@ -1,15 +1,30 @@
 import React from 'react';
-import Table from './Table';
-import Thead from './Thead';
+// import React, { useEffect } from 'react';
+import Table from '../../../components/Table';
+import Tbody from '../../../components/Tbody';
+import Thead from '../../../components/Thead';
+import useManagerUsersContext from '../../../hooks/useManagerUsersContext';
+import usersSerializeFiltered from '../../../utils/usersSerializeFiltered';
 
-const TableRegisteredUsers = () => (
-  <Table>
-    <Thead
-      theadList={
-        ['Item', 'Nome', 'E-mail', 'Tipo', 'Excluir']
-      }
-    />
-  </Table>
-);
+const TableRegisteredUsers = () => {
+  const { users } = useManagerUsersContext();
+
+  // useEffect(() => {
+  //   console.log(users);
+  // }, [users]);
+
+  const arrayUsers = usersSerializeFiltered(users);
+
+  return (
+    <Table>
+      <Thead
+        theadList={
+          ['Item', 'Nome', 'E-mail', 'Tipo', 'Excluir']
+        }
+      />
+      <Tbody tbodyList={ arrayUsers } />
+    </Table>
+  );
+};
 
 export default TableRegisteredUsers;
